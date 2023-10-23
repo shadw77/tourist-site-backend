333be5e90cd3a917095955f5c79888759e5a4afa
alaanegm <alaanegm275@gmail.com>
2023-10-23 20:33:54 +0300

    hotel api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\api\HotelController;
+use App\Http\Controllers\api\HotelImageController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('hotels', HotelController::class);
+Route::apiResource('hotelImages', HotelImageController::class);
