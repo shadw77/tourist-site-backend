668332cc2d7cd102408edc9b9d5267637a9417cc
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-10-25 19:41:43 +0300

    create fake data for images

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +23,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        // Relation::enforceMorphMap([
+        //     'Trip' => 'App\Models\Trip',
+        //     'Hotel' => 'App\Models\Hotel',
+        //     'Destination' => 'App\Models\Destination',
+        //     'Restaurant' => 'App\Models\Restaurant',
+        //     'Room' => 'App\Models\Room',
+        // ]);
+        
     }
 }
